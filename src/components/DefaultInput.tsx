1ccaf1dcb9b23c7144bb43284986bffb8b948221
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-06-09 14:30:05 -0300

    feat: Adds responsiveness

--- src/components/DefaultInput.tsx
+++ src/components/DefaultInput.tsx
@@ -8,8 +8,14 @@ export function DefaultInput() {
   }
 
   return (
-    <div className="flex h-full w-full flex-col items-center gap-4">
-      <h2 className="font-bold text-white">Input sem debounce</h2>
+    <div className="flex h-36 w-full flex-col items-center gap-4">
+      <h2 className="font-bold text-white">
+        Input{' '}
+        <span className="underline decoration-[#5c5c5c] decoration-2 underline-offset-2">
+          sem
+        </span>{' '}
+        debounce
+      </h2>
       <input
         className="w-full rounded-md border border-[#404040] bg-[#1c1c1c] p-2 text-sm text-[#e5e5e5] placeholder:text-[#5c5c5c]"
         type="text"
